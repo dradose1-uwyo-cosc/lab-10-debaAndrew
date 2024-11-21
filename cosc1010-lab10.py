da7481560edb8c89628a0878256af103e08bce34
debaAndrew <adeba@uwyo.edu>
2024-11-21 22:43:44 +0000

    deba

--- cosc1010-lab10.py
+++ cosc1010-lab10.py
@@ -3,38 +3,7 @@
 # 11/21/2024
 # Lab 10
 # Lab Section: 18
-# Sources, people worked with, help given to: 
-
-#import modules you will need 
-
-from hashlib import sha256 
-from pathlib import Path
-
-def get_hash(to_hash):
-    """You can use """
-    return sha256(to_hash.encode('utf-8')).hexdigest().upper()
-
-#Stored Hash
-try:
-    stored_hash = get_hash("""You can use """)
-except:
-    print("PLACEHOLDER")
-
-#Compare Against Passwords
-try:
-    path = Path('rockyou.txt')
-    content = (path.read_text()).splitlines()
-    for line in content:
-        #print(line)
-        hash_integral = get_hash(line)
-        print(f"{hash_integral} \n")
-        if hash_integral == stored_hash:
-            print(f"Success on the line: {line}")
-except FileNotFoundError:
-    print("IDK")
-
-
-
+# Sources, people worked with, help given to: John Mays, Kagan Lenzen
 
 # Files and Exceptions
 
@@ -60,3 +29,45 @@ except FileNotFoundError:
 # Hash each individual password and compare it against the stored hash.
 # - When you find the match, print the plaintext version of the password.
 # - End your loop.
+
+#import modules you will need 
+
+from hashlib import sha256 
+from pathlib import Path
+
+def get_hash(to_hash):
+    """You can use """
+    return sha256(to_hash.encode('utf-8')).hexdigest().upper()
+
+#Compare Against Passwords
+def compare_passwords(stored_hash):
+    for line in content:
+        #print(line)
+        hash_integral = get_hash(line)
+        #print(f"{hash_integral} \n")
+        if hash_integral == stored_hash:
+            return line
+    return "not in our database"
+
+#Stored Hash
+try:
+    path = Path('hash')
+    stored_hash = (path.read_text())
+    #print(stored_hash)
+except:
+    print("Error - 'hash' did not successfully load")
+else:
+    print("'hash' is successfully loaded")
+
+#Loading Password Database
+try:
+    #print(stored_hash)
+    path = Path('rockyou.txt')
+    content = (path.read_text())
+except:
+    print("Error - 'rockyou.txt' did not successfully load")
+else:
+    print("'rockyou.txt' is successfully loaded")
+    content = content.splitlines()
+
+print(f"The password that matches the 'hash' value is '{compare_passwords(stored_hash)}'")
